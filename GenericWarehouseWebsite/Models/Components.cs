3558e79df28fdf19f2eff051ee188bde7dbcec3d
baoqt <baoq.tran@outlook.com>
2018-04-24 23:22:21 -0700

    Added components model

---
+++ GenericWarehouseWebsite/Models/Components.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenericWarehouseWebsite.Models
+{
+    public class Components
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Bin { get; set; }
+        public int Quantity { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
---
+++ GenericWarehouseWebsite/Models/ComponentsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenericWarehouseWebsite.Models
+{
+    public class ComponentsContext : DbContext
+    {
+        public ComponentsContext(DbContextOptions<ComponentsContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Components> Components { get; set; }
+    }
+}
