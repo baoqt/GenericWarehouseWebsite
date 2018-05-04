32567eff1ec2d10a0c85194f70bd0efb8ba34c71
tahakittani@gmail.com <tahakittani@gmail.com>
2018-05-04 13:24:27 -0700

    add a product class

---
+++ GenericWarehouseWebsite/products/products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenericWarehouseWebsite.NewFolder
+{
+    public class products
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ProDate { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
