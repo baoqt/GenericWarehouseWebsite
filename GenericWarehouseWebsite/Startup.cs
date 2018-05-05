803cf17738f185a3c009dc3b2a9496c9855308bb
tahakittani@gmail.com <tahakittani@gmail.com>
2018-05-04 18:31:33 -0700

    changed the security of browsing images

--- GenericWarehouseWebsite/Startup.cs
+++ GenericWarehouseWebsite/Startup.cs
@@ -27,6 +27,9 @@ namespace GenericWarehouseWebsite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+           
+                services.AddDirectoryBrowser();
+            
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
