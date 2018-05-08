3a4077ff45e659930daaa2b93d20a95f098dfb7c
Giovanni Garcia <ggarcia4@sdsu.edu>
2018-05-08 14:31:06 -0700

    Added sorting, filtering, and searching to Tools, and Component pages as per the tutorial.

--- GenericWarehouseWebsite/Pages/Components/Index.cshtml.cs
+++ GenericWarehouseWebsite/Pages/Components/Index.cshtml.cs
@@ -19,11 +19,86 @@ namespace GenericWarehouseWebsite.Pages.Components
             _context = context;
         }
 
-        public IList<Component> Component { get; set; }
+        public PaginatedList<Component> Component { get; set; }
+        //Adding Sorting and Filtering functionality
+        public string NameSort { get; set; }
+        public string PartSort { get; set; }
+        public string CostSort { get; set; }
+        public string BinSort { get; set; }
+        public string QTYSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder,
+    string currentFilter, string searchString, int? pageIndex)
         {
-            Component = await _context.Components.ToListAsync();
+            //Sorting
+            CurrentSort = sortOrder;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            PartSort = sortOrder == "PartNumber" ? "PartNumber_desc" : "PartNumber";
+            CostSort = sortOrder == "Cost" ? "Cost_desc" : "Cost";
+            BinSort = sortOrder == "Bin" ? "Bin_desc" : "Bin";
+            QTYSort = sortOrder == "QTY" ? "QTY_desc" : "QTY";
+            //Filtering
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            CurrentFilter = searchString;
+
+            IQueryable<Component> componentIQ = from s in _context.Components
+                                           select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                componentIQ = componentIQ.Where(s => s.Name.Contains(searchString)
+                                || s.PartNumber.Contains(searchString)
+                                || s.Bin.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    componentIQ = componentIQ.OrderByDescending(s => s.Name);
+                    break;
+                case "PartNumer":
+                    componentIQ = componentIQ.OrderBy(s => s.PartNumber);
+                    break;
+                case "PartNumber_desc":
+                    componentIQ = componentIQ.OrderByDescending(s => s.PartNumber);
+                    break;
+                case "Cost":
+                    componentIQ = componentIQ.OrderBy(s => s.Cost);
+                    break;
+                case "Cost_desc":
+                    componentIQ = componentIQ.OrderByDescending(s => s.Cost);
+                    break;
+                case "Bin":
+                    componentIQ = componentIQ.OrderBy(s => s.Bin);
+                    break;
+                case "Bin_desc":
+                    componentIQ = componentIQ.OrderByDescending(s => s.Bin);
+                    break;
+                case "QTY":
+                    componentIQ = componentIQ.OrderBy(s => s.Quantity);
+                    break;
+                case "QTY_desc":
+                    componentIQ = componentIQ.OrderByDescending(s => s.Quantity);
+                    break;
+
+
+                default:
+                    componentIQ = componentIQ.OrderBy(s => s.Name);
+                    break;
+            }
+
+            int pageSize = 3;
+            Component = await PaginatedList<Component>.CreateAsync(
+                componentIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
--- GenericWarehouseWebsite/Pages/Tools/Index.cshtml.cs
+++ GenericWarehouseWebsite/Pages/Tools/Index.cshtml.cs
@@ -19,11 +19,87 @@ namespace GenericWarehouseWebsite.Pages.Tools
             _context = context;
         }
 
-        public IList<Tool> Tool { get; set; }
+        public PaginatedList<Tool> Tool { get; set; }
+        //Adding Sorting and Filtering functionality
+        public string NameSort { get; set; }
+        public string PartSort { get; set; }
+        public string CostSort { get; set; }
+        public string BinSort { get; set; }
+        public string QTYSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder, string currentFilter ,string searchString, int? pageIndex)
         {
-            Tool = await _context.Tools.ToListAsync();
+            //Sorting
+            CurrentSort = sortOrder;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            PartSort = sortOrder == "PartNumber" ? "PartNumber_desc" : "PartNumber";
+            CostSort = sortOrder == "Cost" ? "Cost_desc" : "Cost";
+            BinSort = sortOrder == "Bin" ? "Bin_desc" : "Bin";
+            QTYSort = sortOrder == "QTY" ? "QTY_desc" : "QTY";
+            //Filtering
+            if (searchString != null)//Decided if you are in the index page or some other page then acts accordingly
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            CurrentFilter = searchString;
+
+            IQueryable<Tool> toolIQ = from s in _context.Tools
+                                      select s;
+
+            if(!String.IsNullOrEmpty(searchString))//If the string is valid then tollIQ gets whichever value matches first
+            {
+                toolIQ = toolIQ.Where(s => s.Name.Contains(searchString)
+                                || s.PartNumber.Contains(searchString)
+                                || s.Bin.Contains(searchString));
+            }
+
+
+            switch(sortOrder)//Checls the string from the url and deciedes how to sort it
+            {
+                case "name_desc":
+                    toolIQ = toolIQ.OrderByDescending(s => s.Name);
+                    break;
+                case "PartNumer":
+                    toolIQ = toolIQ.OrderBy(s => s.PartNumber);
+                    break;
+                case "PartNumber_desc":
+                    toolIQ = toolIQ.OrderByDescending(s => s.PartNumber);
+                    break;
+                case "Cost":
+                    toolIQ = toolIQ.OrderBy(s => s.Cost);
+                    break;
+                case "Cost_desc":
+                    toolIQ = toolIQ.OrderByDescending(s => s.Cost);
+                    break;
+                case "Bin":
+                    toolIQ = toolIQ.OrderBy(s => s.Bin);
+                    break;
+                case "Bin_desc":
+                    toolIQ = toolIQ.OrderByDescending(s => s.Bin);
+                    break;
+                case "QTY":
+                    toolIQ = toolIQ.OrderBy(s => s.Quantity);
+                    break;
+                case "QTY_desc":
+                    toolIQ = toolIQ.OrderByDescending(s => s.Quantity);
+                    break;
+
+
+                default:
+                    toolIQ = toolIQ.OrderBy(s => s.Name);
+                    break;
+            }
+            int pageSize = 3;
+
+            Tool = await PaginatedList<Tool>.CreateAsync(
+                toolIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
